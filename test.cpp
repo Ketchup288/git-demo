22be4f76d5a7e1551411de27df21295e74a4b574
Ketchup288 <arsenko525@gmail.com>
2020-11-08 21:42:22 +0200

    Make some fix

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 0;
+    cout << "Input your number here: "; cin >> a;
+    if (a > 0 && a < 200){
+        cout << "Thank's!" << endl;
+    }
+    return 0;
+}
