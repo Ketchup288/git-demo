fdac6f3c171194bbda676234ffbad49c89c4a87e
Ketchup288 <arsenko525@gmail.com>
2020-11-08 21:05:12 +0200

    Fixed one bug

--- test_std.cpp
+++ test_std.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 int main() {
-    cout << "Input some number here" << endl;
     int a = 0;
-    cin >> a;
+    cout << "Input some number here: "; cin >> a;
     cout << "You've inputed: " << a << "\n";
     return 0;
 }
