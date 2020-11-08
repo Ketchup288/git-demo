6fb3a234cf8e8b20f3db229311bf23ec76c12927
Ketchup288 <arsenko525@gmail.com>
2020-11-08 22:51:53 +0200

    New good working file

---
+++ error.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int number = 0;
+    while (number <= 10){
+        number += 1;
+    }
+    cout << number << endl;
+    return 0;
+}
