7e02b00890eda685b7375bf162fbbce037c12420
Ketchup288 <arsenko525@gmail.com>
2020-11-08 22:03:19 +0200

    Fixed error in this file

--- ERROR_FILE.cpp
+++ ERROR_FILE.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main(){
     int number = 100;
     while (number <= 1000){
-        number -= 10;
+        number += 10;
         cout << 'Now your number is: ' << number << endl;
     }
+    cout << "Everything gone ok"<< endl;
     return 0;
 }
