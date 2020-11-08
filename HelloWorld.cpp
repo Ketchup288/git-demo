513befbbd551e9be4bce2ca8b83be7ea025d1806
Arsen <64072919+Ketchup288@users.noreply.github.com>
2020-11-08 16:58:21 +0200

    Update HelloWorld.cpp
    Corrected some mistakes

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
     cout << "Hello World" << endl;
-    number = 1; 
+    int number = 1; 
     while(number<=20)
     {
         number += 1;
         cout << "Now your number is:" << number << endl;
     }
-    return 0
+    return 0;
 }
