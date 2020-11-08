f62384ac002617fc1de7e19a6b40fc0fd1296ff8
Ketchup288 <arsenko525@gmail.com>
2020-11-08 20:53:23 +0200

    Just test new func

---
+++ test_std.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Input some number here" << endl;
+    int a = 0;
+    cin >> a;
+    cout << "You've inputed: " << a << "\n";
+    return 0;
+}
