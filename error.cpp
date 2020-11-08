69d18427594ad919368f3400086a9ed0203870e9
Ketchup288 <arsenko525@gmail.com>
2020-11-08 22:54:10 +0200

    Update file with critical error

--- error.cpp
+++ error.cpp
@@ -6,6 +6,7 @@ int main(){
     int number = 0;
     while (number <= 10){
         number += 1;
+        SOME CRITICAL ERROR && SOME NOT CRITICAL ERROR
     }
     cout << number << endl;
     return 0;
