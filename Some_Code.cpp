fe52b0c9701374514eb63e7fbba087710d049e81
Arsen <64072919+Ketchup288@users.noreply.github.com>
2020-11-08 17:21:35 +0200

    Update Some_Code.cpp
    Change code a bit.

--- Some_Code.cpp
+++ Some_Code.cpp
@@ -10,7 +10,9 @@ int main(){
         int temp_number_3 = number - temp_number_2;
         if (temp_number_3 == 0){
             std::cout << "Your number is: " << temp_number_1 << std::endl;
-        }
+        if (temp_number_3 >= 0) {
+            std::cout << "I can't divivde" << number << " on 256, change your number and try again!" << std::endl;
+        }   
     }
     return 0;
 }
